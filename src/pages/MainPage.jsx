8e372ed687ee22a4e3f523c78c2d51e0feb79731
shuler7 <vitalisobolevggg@gmail.com>
2025-10-23 20:27:50 +0300

    Update main page design

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -7,46 +7,43 @@ import Header from "../components/Header";
 export default function MainPage() {
 
     const headerRef = useRef();
-    const mainPageIntroSectionRef = useRef();
 
     window.addEventListener("scroll", () => {
         if (document.documentElement.scrollTop > 20) {
             headerRef.current.classList.remove('atTheTop');
-            mainPageIntroSectionRef.current.classList.remove('atTheTop');
         } else {
             headerRef.current.classList.add('atTheTop');
-            mainPageIntroSectionRef.current.classList.add('atTheTop');
         };
     });
 
     return (
         <div id="mainPageContainer">
             <Header headerRef={headerRef} />
-            <section id="mainPageIntroSection" className="atTheTop" ref={mainPageIntroSectionRef}>
+            <section id="mainPageIntroSection" className="atTheTop">
                 <h1>Find Your Pet</h1>
                 <div id='mainPageIntroButtonsContainer'>
                     <button
                         className="mainPageIntroButton"
                         style={{
                             border: 'none',
-                            background: "var(--accent-color)",
+                            background: "var(--primary-color)",
                         }}
                     >
-                        <h5>
+                        <h2>
                             Создать объявление
-                        </h5>
+                        </h2>
                     </button>
                     <button
                         className="mainPageIntroButton"
                         style={{
-                            border: 'solid 2px var(--primary-color)',
-                            backdropFilter: 'blur(10px)',
-                            background: "transparent",
+                            border: 'solid 3px var(--primary-color)',
+                            background: "#ffffff60",
+                            backdropFilter: 'blur(8px)'
                         }}
                     >
-                        <h5 style={{ color: 'var(--primary-color)' }}>
-                            Найти объявление
-                        </h5>
+                        <h2 style={{ color: 'var(--primary-color)' }}>
+                            Найти объявления
+                        </h2>
                     </button>
                 </div>
             </section>
